b5e302b8fbfbbb2ad7ad22a21e127d75a9796cd9
Shivam Mukhade <smukhade@redhat.com>
2022-02-16 13:50:03 +0530

    Adds pipeline

---
+++ pipeline/00-rbac.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# create namespace if doesn't exist
+kubectl create namespace news-demo 2>/dev/null || true
+
+# install tasks for pipeline
+kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/git-clone/0.5/git-clone.yaml
+kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/golang-build/0.3/golang-build.yaml
+kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/golang-test/0.2/golang-test.yaml
+kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/buildah/0.3/buildah.yaml
+kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/kubernetes-actions/0.2/kubernetes-actions.yaml
+
+# secret to push image to registry
+kubectl -n news-demo create secret generic registry-secret \
+      --type="kubernetes.io/basic-auth" \
+      --from-literal=username="<add-your-username-here>" \
+      --from-literal=password="<add-your-password-here>"
+
+# annotating registry name to secret
+kubectl -n news-demo annotate secret registry-secret tekton.dev/docker-0=quay.io
+
+# required role for service account to create/get/patch deployment
+kubectl -n news-demo create role news-demo-access \
+    --resource=deployment\
+    --verb=create,patch,get,list
+
+# create a serviceAccount
+cat <<EOF | kubectl -n news-demo create -f-
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: news-demo
+  namespace: news-demo
+secrets:
+  - name: registry-secret
+EOF
+
+# role binding to attach role to serviceAccount
+kubectl -n news-demo create rolebinding news-demo \
+    --serviceaccount=news-demo:news-demo \
+    --role=news-demo-access
+
