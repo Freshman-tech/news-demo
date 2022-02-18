7991e2fb556a9e7bc9fdc0f013a538c7936f6050
Shivam Mukhade <smukhade@redhat.com>
2022-02-18 11:37:15 +0530

    Adds trigger resources
    Signed-off-by: Shivam Mukhade <smukhade@redhat.com>

--- pipeline/run.sh
+++ pipeline/run.sh
@@ -4,18 +4,18 @@
 kubectl create namespace news-demo 2>/dev/null || true
 
 # install tasks for pipeline
-kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/git-clone/0.5/git-clone.yaml
-kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/golang-build/0.3/golang-build.yaml
-kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/golang-test/0.2/golang-test.yaml
-kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/buildah/0.3/buildah.yaml
-kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/kubernetes-actions/0.2/kubernetes-actions.yaml
+kubectl apply -n news-demo -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/git-clone/0.5/git-clone.yaml
+kubectl apply -n news-demo -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/golang-build/0.3/golang-build.yaml
+kubectl apply -n news-demo -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/golang-test/0.2/golang-test.yaml
+kubectl apply -n news-demo -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/buildah/0.3/buildah.yaml
+kubectl apply -n news-demo -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/kubernetes-actions/0.2/kubernetes-actions.yaml
 
 # secret to push image to registry
 kubectl -n news-demo create secret generic registry-secret \
       --type="kubernetes.io/basic-auth" \
       --from-literal=username="<add-your-username-here>" \
       --from-literal=password="<add-your-password-here>"
-
+      
 # annotating registry name to secret
 kubectl -n news-demo annotate secret registry-secret tekton.dev/docker-0=quay.io
 
@@ -40,3 +40,11 @@ kubectl -n news-demo create rolebinding news-demo \
     --serviceaccount=news-demo:news-demo \
     --role=news-demo-access
 
+# openshift specific, not required for any other kubernetes cluster
+oc adm policy add-scc-to-user privileged system:serviceaccount:news-demo:news-demo
+
+# create pipeline
+kubectl create -n news-demo -f ./pipeline/01-pipeline.yaml
+
+# create pipelineRun
+kubectl create -n news-demo -f ./pipeline/02-pipelinerun.yaml
